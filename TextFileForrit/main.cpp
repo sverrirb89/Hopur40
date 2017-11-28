77da33a813c1aef994f100bcc960be8cf55bb35b
Your Name <you@example.com>
2017-11-28 17:04:10 +0000

    new files

---
+++ TextFileForrit/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+int main() {
+
+    string str;
+    ifstream fin;
+    char answer;
+
+    fin.open("TextFile.txt");
+
+   do {
+    if (fin.is_open()) {
+       for(int i = 0; i < 10; i++) {
+        getline(fin, str);
+
+        cout << str << endl;
+        }
+    }
+
+    else {
+        cout << "Unable to read from file!" << endl;
+    }
+
+        do {
+            cout << "Continue? (y/n)" << endl;
+            cin >> answer;
+        } while (answer != 'y' && answer != 'n');
+   } while (answer != 'n');
+
+   fin.close();
+
+    return 0;
+}
