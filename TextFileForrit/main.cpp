709e3d660ca12c6b6a922f2504d97e760b754d0a
Your Name <you@example.com>
2017-12-06 18:05:57 +0000

    updating my directory

--- TextFileForrit/main.cpp
+++ TextFileForrit/main.cpp
@@ -4,34 +4,34 @@ using namespace std;
 
 
 int main() {
-    
+
     string str;
     ifstream fin;
     char answer;
-    
-    fin.open("TextFile.txt");
-    
+
+    fin.open("TextFile.txt");                   // Opna texta skjalið
+
     do {
-        if (fin.is_open()) {
-            for(int i = 0; i < 10; i++) {
-                getline(fin, str);
-                
-                cout << str << endl;
+        if (fin.is_open()) {                    // Ef skráin er opin
+            for(int i = 0; i < 10; i++) {       // Ýtra gegnum línurnar
+                getline(fin, str);              // Fá línuna
+
+                cout << str << endl;            // Prenta út línurnar
             }
         }
-        
+
         else {
             cout << "Unable to read from file!" << endl;
         }
-        
+
         do {
             cout << "Continue? (y/n)" << endl;
             cin >> answer;
         } while (answer != 'y' && answer != 'n');
     } while (answer != 'n');
-    
+
     fin.close();
-    
+
     return 0;
 }
 
