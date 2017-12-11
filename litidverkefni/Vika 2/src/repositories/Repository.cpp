700b0829de3fb55f95420ab2bc9770d0f31bab6c
Your Name <you@example.com>
2017-12-11 17:36:25 +0000

    new comments

--- litidverkefni/Vika 2/src/repositories/Repository.cpp
+++ litidverkefni/Vika 2/src/repositories/Repository.cpp
@@ -22,12 +22,15 @@ void Repository::add_payslip(const PaySlip& payslip){
 
 void Repository::read_from_file(){
     ifstream fin;
+    //breyta nafninu á skránni
     fin.open("Payslips.txt");
     all_pay_slips.clear();
     if (fin.is_open()){
         while(!fin.eof()){
+            //Annar klasi kæmi hingað inn (t.d. OrderModalClass klasinn sem geymir upplýsingar um allar pantanirnar)
             PaySlip P;
             string file;
+            //fin >> file les inn næstu línu í skjalinu ykkar í stringinn file í gegnum while lúppuna
             fin >> file;
 
             stringstream ss(file);
@@ -59,6 +62,7 @@ void Repository::read_from_file(){
                     P.set_month(x);
                 }
                 else if (counter == 4){
+                    //Í OrderModalClass klasanum kallið þið aftur á getline með delimeterinum '-' t.d.
                     string s = item;
                     stringstream testing(s);
                     int x = 0;
@@ -78,17 +82,23 @@ void Repository::read_from_file(){
     cout << "Can you see me " << endl;
 }
 
+//Réttast er að hafa þetta fall í services klasanum :D
+//Services klasinn kallar í read_from_file fallið sem myndi þá ekki vera void
+//heldur return vector<PaySlip>
+//Þá myndi þetta fall vera í services klasanum
 vector<PaySlip> Repository::withSameSsn(string ssn){
+    //Populate the vector
     read_from_file();
+    //Clear the vector so it´s empty when we start to fill it
     paySlipsWithSameSsn.clear();
 
+    //for loop to find the data we need from the vector with all the data from the file
     for (int i = 0; i < all_pay_slips.size(); i++){
         if (all_pay_slips[i].get_ssn() == ssn){
             paySlipsWithSameSsn.push_back(all_pay_slips[i]);
         }
     }
-    cout << "Length of vector I want to return is: " << paySlipsWithSameSsn.size() << endl;
-
+    //Return the data we were searching for up to services and then the ui
     return paySlipsWithSameSsn;
 }
 
